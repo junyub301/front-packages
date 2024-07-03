cd26141f633e1a68925bee95429ae48178044f4e
junyub301 <junyub0301@gmail.com>
2024-07-03 21:33:42 +0900

    Feat: 레이아웃 컴포넌트 - Divider

--- packages/react/components/layout/src/layout/types.ts
+++ packages/react/components/layout/src/layout/types.ts
@@ -1,3 +1,12 @@
+import { vars } from "@study/themes";
 import { AsElementProps, StyleProps } from "../core/types";
+import React from "react";
 
 export type BoxProps = AsElementProps & StyleProps;
+
+export type DividerProps = {
+  orientation?: "horizontal" | "vertical";
+  color?: keyof typeof vars.colors.$scale;
+  size?: number;
+  variant?: "solid" | "dashed";
+} & React.HTMLAttributes<HTMLHRElement>;
