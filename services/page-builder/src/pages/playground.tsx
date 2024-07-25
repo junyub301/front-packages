9249c9792c9462b7b08033b3f720f18b5a320e23
junyub301 <junyub0301@gmail.com>
2024-07-25 22:10:06 +0900

    Feat: page-builder 환경설정 part2

--- services/page-builder/src/pages/playground.tsx
+++ services/page-builder/src/pages/playground.tsx
@@ -1,8 +1,13 @@
+import { vars } from "@study/themes";
+import { Box } from "@study/react-components-layout";
+import { CDN_BASE_URL } from "../constants";
+
 const PlaygroundPage = () => {
   return (
-    <div>
-      <h1>Playground</h1>
-    </div>
+    <Box padding={3}>
+      <h1>{vars.colors.$scale.gray[100]}</h1>
+      <img src={`${CDN_BASE_URL}/test/hero_mobile.webp`} />
+    </Box>
   );
 };
 
