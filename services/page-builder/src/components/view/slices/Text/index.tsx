c599ec9575115a859e276e69f85686b70a807245
junyub301 <junyub0301@gmail.com>
2024-07-30 21:45:37 +0900

    Feat: 혜택 Section 추가

--- services/page-builder/src/components/view/slices/Text/index.tsx
+++ services/page-builder/src/components/view/slices/Text/index.tsx
@@ -3,6 +3,7 @@ import { vars } from "@study/themes";
 
 type Props = {
   text: string;
+  highlightTexts?: string[];
   sliceStyle?: {
     padding?: keyof typeof vars.box.spacing;
     paddingX?: keyof typeof vars.box.spacing;
@@ -12,10 +13,16 @@ type Props = {
     textSize?: keyof typeof vars.typography.fontSize;
     textWeight?: keyof typeof vars.typography.fontWeight;
     textAlign?: "left" | "right" | "center";
+    highlightTextColor?: string;
+    highlightTextWeight?: keyof typeof vars.typography.fontWeight;
   };
 };
 
-export const TextSlice: React.FC<Props> = ({ text, sliceStyle }: Props) => {
+export const TextSlice: React.FC<Props> = ({
+  text,
+  highlightTexts = [],
+  sliceStyle,
+}: Props) => {
   const {
     padding = 2,
     paddingX = 2,
@@ -25,7 +32,25 @@ export const TextSlice: React.FC<Props> = ({ text, sliceStyle }: Props) => {
     textSize,
     textWeight,
     textAlign = "center",
+    highlightTextColor = vars.colors.$static.light.yellow[400],
+    highlightTextWeight,
   } = sliceStyle ?? {};
+
+  const regex = new RegExp(`(${highlightTexts?.join("|")})`, "gi");
+  const highlightedText = text.split(regex).map((word, index) => {
+    if (highlightTexts.some((query) => new RegExp(query, "i").test(word))) {
+      return (
+        <span
+          key={`${word}-${index}`}
+          style={{ color: highlightTextColor, fontWeight: highlightTextWeight }}
+        >
+          {word}
+        </span>
+      );
+    }
+    return word;
+  });
+
   return (
     <Text
       fontSize="2xl"
@@ -38,9 +63,11 @@ export const TextSlice: React.FC<Props> = ({ text, sliceStyle }: Props) => {
         fontSize: textSize,
         fontWeight: textWeight,
         textAlign,
+        whiteSpace: "pre-wrap",
+        wordBreak: "keep-all",
       }}
     >
-      {text}
+      {highlightedText}
     </Text>
   );
 };
