55c9d461b9edde39334006c32cafd7cf1732abbe
junyub301 <junyub0301@gmail.com>
2024-08-05 21:35:56 +0900

    Feat: page-builder monaco editor를 이용한 JSON editor 추가

---
+++ services/page-builder/src/components/Editor/Json.tsx
@@ -0,0 +1,15 @@
+import { Editor, EditorProps } from "@monaco-editor/react";
+
+type Props = EditorProps;
+
+export const JsonEditor = (props: Props) => {
+  const { defaultLanguage = "json", height = "90vh", options, ...rest } = props;
+  return (
+    <Editor
+      {...rest}
+      height={height}
+      defaultLanguage={defaultLanguage}
+      options={{ fontSize: 16, formatOnPaste: true, ...options }}
+    />
+  );
+};
--- services/page-builder/src/pages/editor/new.tsx
+++ services/page-builder/src/pages/editor/new.tsx
@@ -2,13 +2,23 @@ import { DesktopFirstLayout } from "@/src/components/layout/DesktopFirstLayout";
 import { DesktopFirstBody } from "@/src/components/layout/DesktopFirstLayout/Body";
 import { DesktopFirstNav } from "@/src/components/layout/DesktopFirstLayout/Nav";
 import { Button } from "@study/react-components-button";
-import React from "react";
+import React, { useState } from "react";
+import { JsonEditor } from "@/src/components/Editor/Json";
+import { ViewSliceSchemaSnippet } from "@/src/utils/jsonEditor/ViewSchemaSnippet";
+import { formatObjectToJson } from "@/src/utils/jsonEditor";
 
 export default function EditorNewPage() {
+  const [schema, setSchema] = useState(
+    formatObjectToJson(ViewSliceSchemaSnippet.init),
+  );
+
+  const handleReset = () => {
+    setSchema(formatObjectToJson(ViewSliceSchemaSnippet.init));
+  };
   return (
     <DesktopFirstLayout>
       <DesktopFirstNav gap={8}>
-        <Button size="md" color="red" variant="outline">
+        <Button size="md" color="red" variant="outline" onClick={handleReset}>
           초기화
         </Button>
         <Button size="md" color="gray" variant="outline">
@@ -18,7 +28,12 @@ export default function EditorNewPage() {
           배포하기
         </Button>
       </DesktopFirstNav>
-      <DesktopFirstBody>Body</DesktopFirstBody>
+      <DesktopFirstBody>
+        <JsonEditor
+          value={schema}
+          onChange={(value) => setSchema(value || "")}
+        />
+      </DesktopFirstBody>
     </DesktopFirstLayout>
   );
 }
