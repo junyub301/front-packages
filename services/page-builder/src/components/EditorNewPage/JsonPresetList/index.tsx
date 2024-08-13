1d7b11bc15f32e48f23e0b85e507bb786a73858a
junyub301 <junyub0301@gmail.com>
2024-08-13 21:36:53 +0900

    Feat: page-builder Json preset

---
+++ services/page-builder/src/components/EditorNewPage/JsonPresetList/index.tsx
@@ -0,0 +1,84 @@
+import { ValidateViewSchemaProps } from "@/src/hooks/useViewSchemaValidation";
+import { formatObjectToJson } from "@/src/utils/jsonEditor";
+import { ViewSliceSchemaSnippet } from "@/src/utils/jsonEditor/ViewSchemaSnippet";
+import { Button } from "@study/react-components-button";
+import { useToast } from "@study/react-components-toast";
+import { Divider } from "@study/react-components-layout";
+import { Fragment } from "react";
+type Props = {
+  schema: string;
+  setSchema: (schema: string) => void;
+  validateViewSchema: (props: ValidateViewSchemaProps) => void;
+};
+
+type Preset = {
+  name: string;
+  snippet: object;
+};
+export const JsonPresetList = ({
+  schema,
+  setSchema,
+  validateViewSchema,
+}: Props) => {
+  const { toast } = useToast();
+
+  const presets: Preset[] = [
+    {
+      name: "TextSlice",
+      snippet: ViewSliceSchemaSnippet.textSlice,
+    },
+    {
+      name: "ImageSlice",
+      snippet: ViewSliceSchemaSnippet.imageSlice,
+    },
+    {
+      name: "ImageSliderSectionSlice",
+      snippet: ViewSliceSchemaSnippet.imageSliderSectionSlice,
+    },
+    {
+      name: "SpacingSlice",
+      snippet: ViewSliceSchemaSnippet.spacingSlice,
+    },
+    {
+      name: "accordionSlice",
+      snippet: ViewSliceSchemaSnippet.accordionSlice,
+    },
+  ];
+  return (
+    <>
+      {presets.map(({ name, snippet }) => {
+        const handleClick = () => {
+          validateViewSchema({
+            viewSchema: schema,
+            onSuccess: () => {
+              const objectifiedSchema = JSON.parse(schema);
+              objectifiedSchema.slices.push(snippet);
+
+              setSchema(formatObjectToJson(objectifiedSchema));
+            },
+            onError: ({ message }) => {
+              toast({
+                payload: {
+                  message,
+                },
+              });
+            },
+          });
+        };
+
+        return (
+          <Fragment key={name}>
+            <Button
+              style={{ borderRadius: "0", width: "100%" }}
+              variant="ghost"
+              onClick={handleClick}
+            >
+              {name}
+            </Button>
+            <Divider />
+          </Fragment>
+        );
+      })}
+    </>
+  );
+};
