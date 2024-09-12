f557728fc17bfa0bdb9619d921ce988f9803300b
junyub301 <junyub0301@gmail.com>
2024-09-12 21:53:59 +0900

    Feat: Typography part 3

--- services/page-builder/src/utils/validation/schema/style.ts
+++ services/page-builder/src/utils/validation/schema/style.ts
@@ -1,17 +1,24 @@
 import { vars } from "@study/themes";
 import { z } from "zod";
 
-const spacingKeys = Object.keys(vars.box.spacing).map((key) =>
-  z.literal(parseInt(key)),
-);
+const spacingKeys = Object.keys(vars.box.spacing).map((key) => z.literal(parseInt(key)));
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const spacingSchema = z.union([...spacingKeys]);
 
 export const commonSliceStyleSchema = z.object({
-  padding: spacingSchema.optional(),
-  paddingX: spacingSchema.optional(),
-  paddingY: spacingSchema.optional(),
+  padding: z.preprocess(
+    (char) => (typeof char === "string" ? parseInt(z.string().parse(char)) : char),
+    spacingSchema.optional(),
+  ),
+  paddingX: z.preprocess(
+    (char) => (typeof char === "string" ? parseInt(z.string().parse(char)) : char),
+    spacingSchema.optional(),
+  ),
+  paddingY: z.preprocess(
+    (char) => (typeof char === "string" ? parseInt(z.string().parse(char)) : char),
+    spacingSchema.optional(),
+  ),
   backgroundColor: z.string().optional(),
 });
