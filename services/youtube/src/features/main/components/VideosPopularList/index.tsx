2b1994134d59d3deef483438b761d4a835ec0601
junyub301 <junyub0301@gmail.com>
2024-10-29 20:38:48 +0900

    Feat: react-query part 2

--- services/youtube/src/features/main/components/VideosPopularList/index.tsx
+++ services/youtube/src/features/main/components/VideosPopularList/index.tsx
@@ -1,13 +1,21 @@
+"use client";
 import { vars } from "@study/themes";
-import { getVideosPopularList } from "@/src/features/main/api/getVideosPopularList";
+import { useGetVideosPopularList } from "../../hooks/useGetVideosPopularList";
+import { useEffect } from "react";
 
-export const VideosPopularList = async () => {
-  const data = await getVideosPopularList({});
+export const VideosPopularList = () => {
+  const { data, fetchNextPage, isFetchingNextPage } = useGetVideosPopularList({});
+  useEffect(() => {
+    setTimeout(() => {
+      fetchNextPage();
+    }, 2000);
+  }, []);
   return (
     <section style={{ color: vars.colors.$scale.gray[900] }}>
       <h2>목록</h2>
       <br />
-      <p>{data.lists?.[0].title}</p>
+      <p>fetching: {isFetchingNextPage ? "true" : "false"}</p>
+      <p>{data.pages[0].lists?.[0].title}</p>
     </section>
   );
 };
