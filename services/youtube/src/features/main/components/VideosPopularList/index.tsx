8aa6e6bbeb1473350b4bc20253541d522f7ef947
junyub301 <junyub0301@gmail.com>
2024-10-31 22:04:49 +0900

    Feat: 인기 동영상 목록 섹션 만들기 part 2

--- services/youtube/src/features/main/components/VideosPopularList/index.tsx
+++ services/youtube/src/features/main/components/VideosPopularList/index.tsx
@@ -1,18 +1,28 @@
 "use client";
+import { VisibilityLoader } from "@/src/shared/components/VisibilityLoader";
 import { useGetVideosPopularList } from "../../hooks/useGetVideosPopularList";
 import { VideosPopularListItem } from "./ListItem";
 import * as s from "./style.css";
 
 export const VideosPopularList = () => {
-  const { data } = useGetVideosPopularList({});
+  const { data, fetchNextPage, isFetchingNextPage, hasNextPage } = useGetVideosPopularList({});
 
   const flatData = data.pages.map((page) => page.lists ?? []).flat();
 
   return (
-    <section className={s.wrapper}>
-      {flatData.map((item) => (
-        <VideosPopularListItem video={item} key={item.videoId} />
-      ))}
-    </section>
+    <>
+      <section className={s.wrapper}>
+        {flatData.map((item) => (
+          <VideosPopularListItem video={item} key={item.videoId} />
+        ))}
+      </section>
+      {hasNextPage && (
+        <VisibilityLoader
+          callback={() => {
+            !isFetchingNextPage && fetchNextPage();
+          }}
+        />
+      )}
+    </>
   );
 };
---
+++ services/youtube/src/shared/components/base/Spinner/index.tsx
@@ -0,0 +1,28 @@
+import { vars } from "@study/themes";
+import * as s from "./style.css";
+import { assignInlineVars } from "@vanilla-extract/dynamic";
+
+type Props = {
+  size?: number;
+  color?: string;
+};
+
+export const Spinner = ({ size = 24, color = vars.colors.$scale.gray[900] }: Props) => (
+  <div
+    className={s.wrapper}
+    style={{
+      ...assignInlineVars({
+        [s.sizeVar]: `${size}px`,
+      }),
+    }}
+  >
+    <div
+      className={s.inner}
+      style={{
+        ...assignInlineVars({
+          [s.innerBorderColorVar]: color,
+        }),
+      }}
+    ></div>
+  </div>
+);
