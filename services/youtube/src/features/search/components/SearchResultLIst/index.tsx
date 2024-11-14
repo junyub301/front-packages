e5e5d3ca8840409435aa38ddf2bdb890622cece9
junyub301 <junyub0301@gmail.com>
2024-11-14 20:57:15 +0900

    Feat: 검색결과 목록 섹션 part 1

---
+++ services/youtube/src/features/search/components/SearchResultLIst/index.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { useGetSearchVideosList } from "../../hooks/useGetSearchVideosList";
+import * as s from "./style.css";
+import { SearchOrder } from "../../api/getSearchVideosList";
+export const SearchResultList = () => {
+  const searchParams = useSearchParams();
+  const searchQuery = {
+    q: searchParams.get("q") ?? "",
+    order: (searchParams.get("order") ?? "relevance") as SearchOrder,
+  };
+
+  const { data } = useGetSearchVideosList(searchQuery);
+  const flatData = data?.pages?.map((page) => page?.lists ?? []).flat();
+  return (
+    <section className={s.container}>
+      <div className={s.wrapper}>{flatData[0]?.title}</div>
+    </section>
+  );
+};
