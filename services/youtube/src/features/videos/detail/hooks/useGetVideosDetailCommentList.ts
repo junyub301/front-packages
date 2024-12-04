d5fcd617f38209d2cc48386e967d303b71b3a543
junyub301 <junyub0301@gmail.com>
2024-12-04 20:54:54 +0900

    Feat: 댓글 목록 - 데이터 가져오기

---
+++ services/youtube/src/features/videos/detail/hooks/useGetVideosDetailCommentList.ts
@@ -0,0 +1,27 @@
+import {
+  InfiniteData,
+  UseSuspenseInfiniteQueryResult,
+  useSuspenseInfiniteQuery,
+} from "@tanstack/react-query";
+import {
+  GetVideosDetailCommentListResponse,
+  GetVideosDetailCommentsListRequestParams,
+  getVideosDetailCommentList,
+} from "../api/getVideosDetailCommentList";
+
+type Params = Pick<GetVideosDetailCommentsListRequestParams, "videoId"> & {
+  initPageToken?: string;
+};
+export const useGetVideosDetailCommentList = ({
+  videoId,
+  initPageToken,
+}: Params): UseSuspenseInfiniteQueryResult<InfiniteData<GetVideosDetailCommentListResponse>, Error> => {
+  return useSuspenseInfiniteQuery({
+    queryKey: ["videoDetail", "commentList", videoId, initPageToken],
+    queryFn: async ({ pageParam = initPageToken }) => {
+      return await getVideosDetailCommentList({ videoId, pageToken: pageParam });
+    },
+    initialPageParam: initPageToken,
+    getNextPageParam: (lastPage) => lastPage.nextPageToken,
+  });
+};
